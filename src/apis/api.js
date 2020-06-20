99371a173a89bf3c5b3aef3a8b66dfd769a4f68c
Karthik HR <karthik.x@hotelsoft.com>
2020-06-20 21:24:11 +0530

    https fix

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,24 +1,22 @@
-const baseUrl = "http://www.omdbapi.com/";
+const baseUrl = "https://www.omdbapi.com/";
 const apikey = "e1075083";
 
 const searchMovies = async (title, year, page) => {
-  try {
-    return await fetch(
-      `${baseUrl}?apiKey=${apikey}&s=${title}&y=${year}&page=${page}&type=movie`
-    ).then((res) => res.json());
-  } catch {
-    return {};
-  }
+	try {
+		return await fetch(`${baseUrl}?apiKey=${apikey}&s=${title}&y=${year}&page=${page}&type=movie`).then((res) =>
+			res.json()
+		);
+	} catch {
+		return {};
+	}
 };
 
 const getMovieInfo = async (imdbID) => {
-  try {
-    return await fetch(
-      `${baseUrl}?apiKey=${apikey}&i=${imdbID}&type=movie`
-    ).then((res) => res.json());
-  } catch {
-    return {};
-  }
+	try {
+		return await fetch(`${baseUrl}?apiKey=${apikey}&i=${imdbID}&type=movie`).then((res) => res.json());
+	} catch {
+		return {};
+	}
 };
 
 export { searchMovies, getMovieInfo };
